e9add338d77218430a3dbb3f8c127fff787b7832
IvoPetk0v <ipetk0v@students.softuni.bg>
2022-05-26 19:02:06 +0300

    uploading homeworks

---
+++ Exercise-Multidimensional Arrays/5. Snake Moves/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5._Snake_Moves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixSize = Console.ReadLine()
+                            .Split(" ")
+                            .Select(int.Parse)
+                            .ToArray();
+            char[,] matrix = new char[matrixSize[0], matrixSize[1]];
+            string input = Console.ReadLine();
+            Queue<char> snake = new Queue<char>();
+            foreach (char ch in input)
+            {
+                snake.Enqueue(ch);
+            }
+            Queue<char> snakeInMotion = new Queue<char>(snake);
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                if ((row + 1) % 2 != 0)
+                {
+                    for (int col = 0; col < matrix.GetLength(1); col++)
+                    {
+                        if (snakeInMotion.Count == 1)
+                        {
+                            matrix[row, col] = snakeInMotion.Dequeue();
+                            snakeInMotion = new Queue<char>(snake);
+                        }
+                        else
+                        {
+                            matrix[row, col] = snakeInMotion.Dequeue();
+                        }
+                    }
+                }
+                else
+                {
+                    for (int col = matrix.GetLength(1)-1; col >= 0; col--)
+                    {
+                        if (snakeInMotion.Count == 1)
+                        {
+                            matrix[row, col] = snakeInMotion.Dequeue();
+                            snakeInMotion = new Queue<char>(snake);
+                        }
+                        else
+                        {
+                            matrix[row, col] = snakeInMotion.Dequeue();
+                        }
+                    }
+                }
+            }
+            PrintMatrix(matrix);
+        }
+        static void PrintMatrix(char[,] matrix)
+        {
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                {
+                    Console.Write(matrix[rows, cols]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Exercise-Multidimensional Arrays/6. Jagged Array Manipulator/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+namespace _6._Jagged_Array_Manipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int[][] jaggedArray = new int[rows][];
+            for (int row = 0; row < rows; row++)
+            {
+                jaggedArray[row] = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            }
+            for (int row = 0; row < rows - 1; row++)
+            {
+                if (jaggedArray[row].Length == jaggedArray[row + 1].Length)
+                {
+                    jaggedArray[row] = jaggedArray[row].Select(el => el * 2).ToArray();
+                    jaggedArray[row + 1] = jaggedArray[row + 1].Select(el => el * 2).ToArray();
+                }
+                else
+                {
+                    jaggedArray[row] = jaggedArray[row].Select(el => el / 2).ToArray();
+                    jaggedArray[row + 1] = jaggedArray[row + 1].Select(el => el / 2).ToArray();
+                }
+            }
+            string command = Console.ReadLine();
+            while (command != "End")
+            {
+                int row = int.Parse(command.Split()[1]);
+                int col = int.Parse(command.Split()[2]);
+                int value = int.Parse(command.Split()[3]);
+                if (command.StartsWith("Add"))
+                {
+                    if (row >= 0 && row < rows && col >= 0 && col < jaggedArray[row].Length)
+                    {
+                        jaggedArray[row][col] = jaggedArray[row][col] + value;
+                    }
+                }
+                else if (command.StartsWith("Subtract"))
+                {
+                    if (row >= 0 && row < rows && col >= 0 && col < jaggedArray[row].Length)
+                    {
+                        jaggedArray[row][col] = jaggedArray[row][col] - value;
+                    }
+                }
+                command = Console.ReadLine();
+            }
+            foreach (int[] row in jaggedArray)
+            {
+                Console.WriteLine(String.Join(' ', row));
+            }
+        }
+    }
+}
