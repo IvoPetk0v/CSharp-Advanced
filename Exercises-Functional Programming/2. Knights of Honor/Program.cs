77342243a3ed00a54f9fd0a054dfd619f976ea5c
IvoPetk0v <ipetk0v@students.softuni.bg>
2022-06-03 22:13:20 +0300

    uploading homeworks

---
+++ Exercises-Functional Programming/2. Knights of Honor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _2._Knights_of_Honor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().Select(x => x = "Sir " + x).ToArray();
+            Action<string> print = message => Console.WriteLine(message);
+            foreach (var word in input)
+            {
+                print(word);
+            }
+        }
+    }
+}
