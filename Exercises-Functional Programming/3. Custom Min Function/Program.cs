a26aa57b14f8317d2258ad623f243caec4f544a9
IvoPetk0v <ipetk0v@students.softuni.bg>
2022-06-03 22:13:58 +0300

    upload homeworks

---
+++ Exercises-Functional Programming/3. Custom Min Function/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                 .Split(" ")
+                 .Select(int.Parse)
+                 .ToArray();
+
+            Func<int[], int> minNum = x => x.Min();
+            Console.WriteLine(minNum(numbers));
+        }
+    }
+}
