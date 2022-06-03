4623e1f7bc38e555e6b57115b57e1b645bb38d1e
IvoPetk0v <ipetk0v@students.softuni.bg>
2022-06-03 22:39:40 +0300

    uploading homeworks

---
+++ Exercises-Functional Programming/4. Find Evens or Odds/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4._Find_Evens_or_Odds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] range = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+            List<int> numbers = new List<int>();
+            for (int i = range[0]; i <= range[1]; i++)
+            {
+                numbers.Add(i);
+            }
+            string cmd = Console.ReadLine();
+            Predicate<int> isEven = number => number % 2 == 0;
+            if (cmd=="even")
+            {
+                numbers = numbers.Where(x => isEven(x)).ToList();
+            }
+            else
+            {
+                numbers = numbers.Where(x => !isEven(x)).ToList();
+            }
+            Console.WriteLine(String.Join(" ",numbers));
+        }
+    }
+}
---
+++ Exercises-Functional Programming/5. Applied Arithmetics/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _5._Applied_Arithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                 .Split(" ")
+                 .Select(int.Parse)
+                 .ToArray();
+            string input;
+            Action<int[]> printNums = numbers => Console.WriteLine(String.Join(" ", numbers));
+            while ((input = Console.ReadLine()) != "end")
+            {
+                if (input == "add")
+                {
+                    numbers = numbers.Select(x => x = x + 1).ToArray();
+                }
+                else if (input == "multiply")
+                {
+                    numbers = numbers.Select(x => x = x * 2).ToArray();
+                }
+                else if (input == "subtract")
+                {
+                    numbers = numbers.Select(x => x = x - 1).ToArray();
+                }
+                else if (input == "print")
+                {
+                    printNums(numbers);
+                }
+            }
+        }
+    }
+}
