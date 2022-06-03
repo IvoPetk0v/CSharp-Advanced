496cfeb91feef1c9d9e5c3aaec16d31167e74de1
IvoPetk0v <ipetk0v@students.softuni.bg>
2022-06-03 21:54:36 +0300

    Uploading homework

---
+++ Exercises-Functional Programming/1. Action Point/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Action_Point
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().ToArray();
+            Action<string> print = message => Console.WriteLine(message);
+            foreach (var word in input)
+            {
+                print(word);
+            }
+
+        }
+
+    }
+}
