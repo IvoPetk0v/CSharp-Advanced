f887d0f9a9bc894d31b1339fa60fa64485e13437
IvoPetk0v <ipetk0v@students.softuni.bg>
2022-06-11 14:56:35 +0300

    upload homeworks

---
+++ Exercise-Generics/2.Generic Box of Integer/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Box
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int numOfInputs = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numOfInputs; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                Box<int> box = new Box<int>(input);
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
