2e88c5d8c1649986a51ffd514d002b320e1b110b
IvoPetk0v <ipetk0v@students.softuni.bg>
2022-06-11 15:35:40 +0300

    upload homework

---
+++ Exercise-Generics/4.Generic Swap Method Integers/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Box
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int numOfInputs = int.Parse(Console.ReadLine());
+            var list = new List<int>();
+            for (int i = 0; i < numOfInputs; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                list.Add(input);
+            }
+            var box = new Box<int>(list);
+            var cmd = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            box.SwapElements(list, cmd[0], cmd[1]);
+            Console.WriteLine(box);
+        }
+    }
+}
