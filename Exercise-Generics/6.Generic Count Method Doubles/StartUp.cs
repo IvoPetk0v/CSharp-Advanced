a14d96592bb899eb8498458a4b0e819b890eb328
IvoPetk0v <ipetk0v@students.softuni.bg>
2022-06-11 16:30:32 +0300

    uploading homework

---
+++ Exercise-Generics/6.Generic Count Method Doubles/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Box
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int numOfInputs = int.Parse(Console.ReadLine());
+            var list = new List<double>();
+            for (int i = 0; i < numOfInputs; i++)
+            {
+                double input = double.Parse(Console.ReadLine());
+                list.Add(input);
+            }
+            var box = new Box<double>(list);
+            double elementToCompare = double.Parse(Console.ReadLine());
+            int count = box.CountGreaterThanElements(list, elementToCompare);
+            Console.WriteLine(count);
+        }
+    }
+}
