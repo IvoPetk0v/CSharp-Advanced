305f0c3ea29f26ad19334594cdb3319f6e47766b
IvoPetk0v <ipetk0v@students.softuni.bg>
2022-06-11 16:57:47 +0300

    upload homework

---
+++ Exercise-Generics/7.Tuple/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Tuple
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split().ToArray();
+            {
+                string names = input[0] + " " + input[1];
+                string address = input[2];
+                var tupleOne = new Tuple<string, string>(names, address);
+                Console.WriteLine(tupleOne);
+            }
+            input = Console.ReadLine().Split().ToArray();
+            string name = input[0];
+            int beerCount = int.Parse(input[1]);
+            var tupleTwo = new Tuple<string, int>(name, beerCount);
+            Console.WriteLine(tupleTwo);
+
+            input = Console.ReadLine().Split().ToArray();
+            int numOne = int.Parse(input[0]);
+            double numTwo = double.Parse(input[1]);
+            var tupleTree = new Tuple<int, double>(numOne, numTwo);
+            Console.WriteLine(tupleTree);
+
+        }
+    }
+}
---
+++ Exercise-Generics/7.Tuple/Tuple.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tuple
+{
+    public class Tuple<T1, T2>
+    {
+        public Tuple(T1 elementOne, T2 elementTwo)
+        {
+            ElementFirst = elementOne;
+            ElementSecond = elementTwo;
+        }
+        public T1 ElementFirst { get; }
+        public T2 ElementSecond { get; }
+
+        public override string ToString()
+        {
+            return $"{ElementFirst} -> {ElementSecond}";
+        }
+    }
+}
