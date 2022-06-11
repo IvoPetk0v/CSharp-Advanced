db99c547d8fa97ba7b3532c62111ef3a7c10bfae
IvoPetk0v <ipetk0v@students.softuni.bg>
2022-06-11 14:50:33 +0300

    Upload homework

---
+++ Exercise-Generics/1. Generic Box of String/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Box
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int numOfInputs = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numOfInputs; i++)
+            {
+                string input = Console.ReadLine();
+                Box<string> box = new Box<string>(input);
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
