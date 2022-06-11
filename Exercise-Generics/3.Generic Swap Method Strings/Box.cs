a90588e8ad1b98e559261b8542cba0873793fc55
IvoPetk0v <ipetk0v@students.softuni.bg>
2022-06-11 15:28:07 +0300

    uploading homework

---
+++ Exercise-Generics/3.Generic Swap Method Strings/Box.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Box
+{
+    public class Box<T>
+    {
+        public Box(T element)
+        {
+            Element = element;
+        }
+        public Box(List<T> elementsList)
+        {
+            Elements = elementsList;
+        }
+        public void SwapElements(List<T> elementsList, int indexOne, int indexTwo)
+        {
+            var elementFirst = elementsList[indexOne];
+            elementsList[indexOne] = elementsList[indexTwo];
+            elementsList[indexTwo] = elementFirst;
+        }
+        private List<T> Elements { get; }
+        public T Element { get; }
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            foreach (var element in Elements)
+            {
+                sb.AppendLine($"{typeof(T)}: {element}");
+            }
+            return sb.ToString();
+        }
+    }
+}
+
---
+++ Exercise-Generics/3.Generic Swap Method Strings/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Box
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int numOfInputs = int.Parse(Console.ReadLine());
+            var list = new List<string>();
+            for (int i = 0; i < numOfInputs; i++)
+            {
+                string input = Console.ReadLine();
+                list.Add(input);
+            }
+            var box = new Box<string>(list);
+            var cmd = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            box.SwapElements(list, cmd[0], cmd[1]);
+            Console.WriteLine(box);
+        }
+    }
+}
