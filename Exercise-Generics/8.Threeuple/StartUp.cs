b7c0dfc29f7797ef3823ceb1440621112eb9a95b
IvoPetk0v <ipetk0v@students.softuni.bg>
2022-06-11 17:19:26 +0300

    upload homework

---
+++ Exercise-Generics/8.Threeuple/StartUp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace Tuple
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split().ToArray();
+            string names = input[0] + " " + input[1];
+            string address = input[2];
+            string town;
+            if (input.Length > 4)
+            {
+                town = input[3] + " " + input[4];
+            }
+            else
+            {
+                town = input[3];
+            }
+            var threepleOne = new Threeuple<string, string, string>(names, address, town);
+            Console.WriteLine(threepleOne);
+
+            input = Console.ReadLine().Split().ToArray();
+            string name = input[0];
+            int beerCount = int.Parse(input[1]);
+            bool drunk = Drunk(input[2]);
+            var threepleTwo = new Threeuple<string, int, bool>(name, beerCount, drunk);
+            Console.WriteLine(threepleTwo);
+
+            input = Console.ReadLine().Split().ToArray();
+            string firstName = input[0];
+            double accBalace = double.Parse(input[1]);
+            string bankName = input[2];
+            var tupleTree = new Threeuple<string, double, string>(firstName, accBalace, bankName);
+            Console.WriteLine(tupleTree);
+
+        }
+        public static bool Drunk(string state)
+        {
+            if (state == "drunk")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Exercise-Generics/8.Threeuple/Threeuple.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tuple
+{
+    public class Threeuple<T1, T2, T3>
+    {
+        public Threeuple(T1 elementOne, T2 elementTwo, T3 elementTree)
+        {
+            ElementFirst = elementOne;
+            ElementSecond = elementTwo;
+            ElementThree = elementTree;
+        }
+        public T1 ElementFirst { get; set; }
+        public T2 ElementSecond { get; set; }
+        public T3 ElementThree { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.ElementFirst} -> {this.ElementSecond} -> {this.ElementThree}";
+        }
+    }
+}
