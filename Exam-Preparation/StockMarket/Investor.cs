cce1c1e1b4004dd227bbe063f8bf0a483fe62b0f
IvoPetk0v <ipetk0v@students.softuni.bg>
2022-06-19 20:16:56 +0300

    uploading solutions

---
+++ Exam-Preparation/StockMarket/Investor.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace StockMarket
+{
+    public class Investor
+    {
+
+        public List<Stock> Portfolio { get; private set; }
+        public string FullName { get; private set; }
+        public string EmailAddress { get; private set; }
+        public decimal MoneyToInvest { get; private set; }
+        public string BrokerName { get; private set; }
+        public Investor(string fullName, string email, decimal moneyToInvest, string broker)
+        {
+            FullName = fullName;
+            EmailAddress = email;
+            MoneyToInvest = moneyToInvest;
+            BrokerName = broker;
+            Portfolio = new List<Stock>();
+        }
+        public int Count
+        {
+            get { return this.Portfolio.Count; }
+        }
+        public void BuyStock(Stock stock)
+        {
+            if (stock.MarketCapitalization > 10000 && this.MoneyToInvest >= stock.PricePerShare)
+            {
+                Portfolio.Add(stock);
+               this.MoneyToInvest -= stock.PricePerShare;
+            }
+        }
+        public string SellStock(string companyName, decimal sellPrice)
+        {
+            if (!Portfolio.Any(firm=>firm.CompanyName==companyName))
+            {
+                return $"{companyName} does not exist.";
+            }
+            if (Portfolio.Find(firm=>firm.CompanyName==companyName).PricePerShare>sellPrice)
+            {
+                return $"Cannot sell {companyName}.";
+            }
+            Stock companyForSell = Portfolio.Find(firm => firm.CompanyName == companyName);
+            Portfolio.Remove(companyForSell);
+            this.MoneyToInvest += sellPrice ;
+            return $"{companyName} was sold.";
+        }
+        public Stock FindStock(string companyName)
+        {
+            return Portfolio.FirstOrDefault(n => n.CompanyName == companyName);
+        }
+        public Stock FindBiggestCompany()
+        {
+            if (!Portfolio.Any())
+            {
+                return null;
+            }
+
+            return Portfolio.OrderByDescending(v => v.MarketCapitalization).FirstOrDefault();
+        }
+        public string InvestorInformation()
+        {
+            string result;
+            result = $"The investor {FullName} with a broker {BrokerName} has stocks:{ Environment.NewLine}" +String.Join(Environment.NewLine, this.Portfolio);
+            return result;
+        }
+    }
+}
---
+++ Exam-Preparation/StockMarket/Stock.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace StockMarket
+{
+    public class Stock
+    {
+        public string CompanyName { get; private set; }
+        public string Director { get; private set; }
+        public decimal PricePerShare { get; private set; }
+        public int TotalNumberOfShares { get; private set; }
+        public decimal MarketCapitalization { get { return this.PricePerShare * this.TotalNumberOfShares; } set { value = this.MarketCapitalization; } }
+
+        public Stock(string name, string director, decimal pricePerShare, int totalNumOfShares)
+        {
+            CompanyName = name;
+            Director = director;
+            PricePerShare = pricePerShare;
+            TotalNumberOfShares = totalNumOfShares;
+            MarketCapitalization = pricePerShare * totalNumOfShares;
+        }
+        public override string ToString()
+        {
+            string result = $"Company: {this.CompanyName}{Environment.NewLine}" +
+                            $"Director: {this.Director}{Environment.NewLine}" +
+                            $"Price per share: ${this.PricePerShare:f2}{Environment.NewLine}" +
+                            $"Market capitalization: ${this.MarketCapitalization:f2}".TrimEnd().TrimStart();
+            return result;
+        }
+    }
+}
