0d38261027cde4bc5b452c144369ecbc25370cc3
IvoPetk0v <ipetk0v@students.softuni.bg>
2022-06-25 18:11:07 +0300

    upload solution

---
+++ Exercice-Iterators and Comparators/CustomComparator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomComparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                var numbers = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                Func<int, int, int> comparator = (first, second) =>
+                {
+                    if (first % 2 == 0 && second % 2 != 0)
+                    {
+                        return -1;
+                    }
+                    else if (first % 2 != 0 && second % 2 == 0)
+                    {
+                        return 1;
+                    }
+                    else
+                    {
+                        return first.CompareTo(second);
+                    }
+                };
+
+                Array.Sort(numbers, new Comparison<int>(comparator));
+
+                Console.WriteLine(string.Join(" ", numbers));
+            }
+        }
+    }
+}
