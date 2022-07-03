e0b7cc0178b4c263ba98983b743f6d47f5987c8d
IvoPetk0v <ipetk0v@students.softuni.bg>
2022-07-03 10:21:08 +0300

    upload

---
+++ Exam-Advanced/First/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> whites = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+            Queue<int> greys = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+            Dictionary<string, int> locations = new Dictionary<string, int>();
+            while (whites.Count > 0 && greys.Count > 0)
+            {
+                int whiteCurr = whites.Pop();
+                int greyCurr = greys.Dequeue();
+                string spot = null;
+                if (whiteCurr == greyCurr)
+                {
+                    if (whiteCurr + greyCurr == 70)//Wall
+                    {
+                        spot = "Wall";
+                    }
+                    else if (whiteCurr + greyCurr == 60)//Countertop
+                    {
+                        spot = "Countertop";
+                    }
+                    else if (whiteCurr + greyCurr == 50)
+                    {
+                        spot = "Oven";
+                    }
+                    else if (whiteCurr + greyCurr == 40)
+                    {
+                        spot = "Sink";
+                    }
+                    else
+                    {
+                        spot = "Floor";
+                    }
+                    if (locations.ContainsKey(spot))
+                    {
+                        locations[spot] += 1;
+                    }
+                    else
+                    {
+                        locations.Add(spot, 1);
+                    }
+                }
+                else
+                {
+                    whiteCurr = whiteCurr / 2;
+                    whites.Push(whiteCurr);
+                    greys.Enqueue(greyCurr);
+                }
+            }
+            if (whites.Count == 0)
+            {
+                Console.WriteLine("White tiles left: none");
+            }
+            else
+            {
+                Console.WriteLine($"White tiles left: {string.Join(", ", whites)}");
+            }
+            if (greys.Count == 0)
+            {
+                Console.WriteLine("Grey tiles left: none");
+            }
+            else
+            {
+                Console.WriteLine($"Grey tiles left: {string.Join(", ", greys)}");
+            }
+            locations = locations.OrderByDescending(x => x.Value).ThenBy(y => y.Key).ToDictionary(x => x.Key, y => y.Value);
+            foreach (var spot in locations)
+            {
+                Console.WriteLine($"{spot.Key}: {spot.Value}");
+            }
+        }
+    }
+}
---
+++ Exam-Advanced/MyNameIsNeo/Program.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyNameIsNeo
+{
+    class Program
+    {
+        static char[,] ReadMatrixDataFromConsole(char[,] matrix)
+        {
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                char[] numbers = Console.ReadLine().ToCharArray();
+
+                for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                {
+                    matrix[rows, cols] = numbers[cols];
+                }
+            }
+            return matrix;
+        }
+        static int[] VankoOnField(char[,] matrix)
+        {
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                {
+                    if (matrix[rows, cols] == 'V')
+                    {
+                        int[] vankoCoordinates = new int[2] { rows, cols };
+                        return vankoCoordinates;
+                    }
+                }
+            }
+            return null;
+        }
+        static bool IsInRangeOfMatrix(char[,] matrix, int row, int col)
+        {
+            if (row < matrix.GetLength(0) && col < matrix.GetLength(1) && row >= 0 && col >= 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void PrintMatrix(char[,] matrix)
+        {
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                {
+                    Console.Write(matrix[rows, cols] + "");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int numRow = int.Parse(Console.ReadLine());
+            char[,] wall = new char[numRow, numRow];
+            ReadMatrixDataFromConsole(wall);
+            int rodHit = 0, holeDig =1;
+            string cmd;
+            while ((cmd = Console.ReadLine()) != "End")
+            {
+                char spot;
+                int[] vanko = VankoOnField(wall);
+              
+                if (cmd == "up") // row - 1 
+                {
+                    if (IsInRangeOfMatrix(wall, vanko[0] - 1, vanko[1]))
+                    {
+                        spot = wall[vanko[0] - 1, vanko[1]];
+                        if (spot == '-')
+                        {
+                            wall[vanko[0], vanko[1]] = '*';
+                            holeDig++;
+                            wall[vanko[0] - 1, vanko[1]] = 'V';
+                        }
+                        else if (spot == 'C')
+                        {
+                            wall[vanko[0], vanko[1]] = '*';
+                            wall[vanko[0] - 1, vanko[1]] = 'E';
+                            holeDig++;
+                            Console.WriteLine($"Vanko got electrocuted, but he managed to make {holeDig} hole(s).");
+                            PrintMatrix(wall);
+                            return;
+                        }
+                        else if (spot == 'R')
+                        {
+                            Console.WriteLine("Vanko hit a rod!");
+                            rodHit++;                           
+                        }
+                        else if (spot == '*')
+                        {
+                            Console.WriteLine($"The wall is already destroyed at position [{vanko[0] - 1}, {vanko[1]}]!");
+                            wall[vanko[0] - 1, vanko[1]] = 'V';
+                            wall[vanko[0], vanko[1]] = '*';
+                        }
+                    }                  
+                }
+                else if (cmd == "down") //row +1 
+                {
+                    if (IsInRangeOfMatrix(wall, vanko[0] + 1, vanko[1]))
+                    {
+                        spot = wall[vanko[0] + 1, vanko[1]];
+                        if (spot == '-')
+                        {
+                            wall[vanko[0], vanko[1]] = '*';
+                            holeDig++;
+                            wall[vanko[0] + 1, vanko[1]] = 'V';
+                        }
+                        else if (spot == 'C')
+                        {
+                            wall[vanko[0], vanko[1]] = '*';
+                            wall[vanko[0] + 1, vanko[1]] = 'E';
+                            holeDig++;
+                            Console.WriteLine($"Vanko got electrocuted, but he managed to make {holeDig} hole(s).");
+                            PrintMatrix(wall);
+                            return;
+                        }
+                        else if (spot == 'R')
+                        {
+                            Console.WriteLine("Vanko hit a rod!");
+                            rodHit++;  
+                        }
+                        else if (spot == '*')
+                        {
+                            Console.WriteLine($"The wall is already destroyed at position [{vanko[0] + 1}, { vanko[1]}]!");
+                            wall[vanko[0] + 1, vanko[1]] = 'V';
+                            wall[vanko[0], vanko[1]] = '*'; 
+                        }
+                    }               
+                }
+                else if (cmd == "left") // col-1 
+                {
+                    if (IsInRangeOfMatrix(wall, vanko[0], vanko[1] - 1))
+                    {
+                        spot = wall[vanko[0], vanko[1] - 1];
+                        if (spot == '-')
+                        {
+                            wall[vanko[0], vanko[1]] = '*';
+                            holeDig++;
+                            wall[vanko[0], vanko[1] - 1] = 'V';
+                        }
+                        else if (spot == 'C')
+                        {
+                            wall[vanko[0], vanko[1]] = '*';
+                            wall[vanko[0], vanko[1] - 1] = 'E';
+                            holeDig++;
+                            Console.WriteLine($"Vanko got electrocuted, but he managed to make {holeDig} hole(s).");
+                            PrintMatrix(wall);
+                            return;
+                        }
+                        else if (spot == 'R')
+                        {
+                            Console.WriteLine("Vanko hit a rod!");
+                            rodHit++;                         
+                        }
+                        else if (spot == '*')
+                        {
+                            Console.WriteLine($"The wall is already destroyed at position [{vanko[0]}, { vanko[1] - 1}]!");
+                            wall[vanko[0], vanko[1] - 1] = 'V';
+                            wall[vanko[0], vanko[1]] = '*';                        
+                        }
+                    }                 
+                }
+                else if (cmd == "right") // col+1
+                {
+                    if (IsInRangeOfMatrix(wall, vanko[0], vanko[1] + 1))
+                    {
+                        spot = wall[vanko[0], vanko[1] + 1];
+                        if (spot == '-')
+                        {
+                            wall[vanko[0], vanko[1]] = '*';
+                            holeDig++;
+                            wall[vanko[0], vanko[1] + 1] = 'V';
+                        }
+                        else if (spot == 'C')
+                        {
+                            wall[vanko[0], vanko[1]] = '*';
+                            wall[vanko[0], vanko[1] + 1] = 'E';
+                            holeDig++;
+                            Console.WriteLine($"Vanko got electrocuted, but he managed to make {holeDig} hole(s).");
+                            PrintMatrix(wall);
+                            return;
+                        }
+                        else if (spot == 'R')
+                        {
+                            Console.WriteLine("Vanko hit a rod!");
+                            rodHit++;                         
+                        }
+                        else if (spot == '*')
+                        {
+                            Console.WriteLine($"The wall is already destroyed at position [{vanko[0]}, { vanko[1] + 1}]!");
+                            wall[vanko[0], vanko[1] + 1] = 'V';
+                            wall[vanko[0], vanko[1]] = '*';
+                        }
+                    }                 
+                }
+            }
+            Console.WriteLine($"Vanko managed to make {holeDig} hole(s) and he hit only {rodHit} rod(s).");
+            PrintMatrix(wall);
+        }
+    }
+}
